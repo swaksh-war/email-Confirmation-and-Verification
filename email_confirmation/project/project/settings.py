ccd3de45ce28126a69b984defdfe1b457b16ecdd
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2021-08-20 09:17:17 +0530

    Update settings.py

--- email_confirmation/project/project/settings.py
+++ email_confirmation/project/project/settings.py
@@ -131,7 +131,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "ghoshswakshwar@gmail.com"
-EMAIL_HOST_PASSWORD = "chotusir###1234"
+EMAIL_HOST_USER = "THE GMAIL HERE"
+EMAIL_HOST_PASSWORD = "YOUR PASSWORD HERE"
 
 
